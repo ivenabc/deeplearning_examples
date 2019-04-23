dc0f3de3b3d0db914eab9e949f84d98524b630fd
bin <iwu@evolveconsulting.com.hk>
2019-04-23 18:45:52 +0800

    activation

--- basic/01activation.py
+++ basic/01activation.py
@@ -1,8 +1,27 @@
 import numpy as np 
+import matplotlib.pylab as plt
 
+# step_function 阶跃函数
+def step_function_v1(x):
+    y = x > 0
+    return y.astype(np.int)
 
+# 
 def step_function(x):
-    if x > 0:
-        return 1
-    else :
-        return 0
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1/(1+ np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+y = sigmoid(x)
+
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
