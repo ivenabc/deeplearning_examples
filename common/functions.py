ebf3ae9c05d7037e46980355562173604185fad0
ivenabc <hopeicc@163.com>
2019-08-01 12:01:05 +0800

    simple net work

--- common/functions.py
+++ common/functions.py
@@ -1,21 +1,52 @@
-import numpy as np 
+import numpy as np
 
-def sotmax(x):
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))    
+
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
+def softmax(x):
     if x.ndim == 2:
-        x = x.T 
+        x = x.T
         x = x - np.max(x, axis=0)
         y = np.exp(x) / np.sum(np.exp(x), axis=0)
-        print('sum:',np.sum(np.exp(x), axis=0))
+        print('sum:', np.sum(np.exp(x), axis=0))
         return y.T
     x = x - np.max(x)
     return np.exp(x)/np.sum(np.exp(x))
 
 
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
+
+
 if __name__ == '__main__':
     x = np.array([
-        [1,2,3,4],
-        [4,5,6,8]
+        [1, 2, 3, 4],
+        [4, 5, 6, 8]
     ])
 
-    y = sotmax(x)
+    y = softmax(x)
     print('y:', y)
--- dlfs1/01mnist_show.py
+++ dlfs1/01mnist_show.py
@@ -5,8 +5,10 @@ from dataset.mnist import load_mnist
 from PIL import Image
 import numpy as np 
 
+
+# 比如对于一个有 2000 个训练样本的数据集。将 2000 个样本分成大小为 500 的 batch，那么完成一个 epoch 需要 4 个 iteration。
+
 def img_show(img):
-    
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
     pil_img.save('./tmp.png')
